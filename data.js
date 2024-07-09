162e3117f5b7898e6c793c056c6a18ba44e75b23
Martijn <oldmartijntje@gmail.com>
2024-07-09 17:36:21 +0200

    updated the voting url

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const setupData = {
-    votingUrl: '',
+    votingUrl: 'https://www.reddit.com/r/AnarchyChess/comments/1dz5fm8/creating_an_very_accurate_chess_simulator_website/',
     githubRepo: 'https://github.com/oldmartijntje/SocialSelect',
     version: 0
 }
