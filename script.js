7c47067dc5c798c3bd1ceab3116e61fee56c524d
Martijn <oldmartijntje@gmail.com>
2024-07-09 17:14:55 +0200

    less formality

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 window.onload = () => {
     console.log(setupData)
-    const element = document.getElementById('linkToVoting');
-    element.href = setupData.votingUrl;
     const chessboard = new Chessboard('chessboard');
 };
