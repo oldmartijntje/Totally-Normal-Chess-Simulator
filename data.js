a3d7b2bb9000a613b218109038f6b1dcad066346
Martijn <oldmartijntje@gmail.com>
2024-07-19 16:25:38 +0200

    new voting url

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const setupData = {
-    votingUrl: 'https://www.reddit.com/r/AnarchyChess/comments/1e048c0/added_microtransactions_to_my_totally_accurate/',
+    votingUrl: 'https://www.reddit.com/r/AnarchyChess/comments/1e750dz/3_added_lootboxes_to_my_totally_accurate_chess/',
     githubRepo: 'https://github.com/oldmartijntje/SocialSelect',
     version: 2
 }
